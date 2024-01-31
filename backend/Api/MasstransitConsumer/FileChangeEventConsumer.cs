84276e42dffc7b24b4b869449e0a6fda1e76e412
AiSY-Yang <978854518@qq.com>
2024-01-31 11:48:44 +0800

    fix bug

--- backend/Api/MasstransitConsumer/FileChangeEventConsumer.cs
+++ backend/Api/MasstransitConsumer/FileChangeEventConsumer.cs
@@ -28,53 +28,56 @@ namespace Api.MasstransitConsumer
 		///<inheritdoc/>
 		public async Task Consume(ConsumeContext<Batch<FileChangeEvent>> context)
 		{
-			var message = context.Message.First().Message;
-			var maidId = message.MaidId;
-			var maid = maidContext.Maids
-						.AsSplitQuery()
-						.Include(x => x.Project)
-						.Include(x => x.Enums)
-						.ThenInclude(x => x.EnumMembers)
-						.Include(x => x.Classes)
-						.ThenInclude(x => x.Properties)
-						.ThenInclude(x => x.Attributes)
-						.First(x => x.Id == maidId);
-			if (File.Exists(Path.Combine(maid.Project.Path, ".git", "index.lock")) || File.Exists(Path.Combine(maid.Project.Path, ".git", "HEAD.lock")))
+			var messages = context.Message.Select(x => x.Message).Distinct().ToList();
+			foreach (var message in messages)
 			{
-				logger.LogInformation("分支切换中,跳过本次执行");
-				return;
-			}
-			if (!maid.Project.GitBranch.IsNullOrEmpty())
-			{
-				var head = File.ReadLines(Path.Combine(maid.Project.Path, ".git", "HEAD")).First();
-				if (!head.EndsWith(maid.Project.GitBranch))
+				var maidId = message.MaidId;
+				var maid = maidContext.Maids
+							.AsSplitQuery()
+							.Include(x => x.Project)
+							.Include(x => x.Enums)
+							.ThenInclude(x => x.EnumMembers)
+							.Include(x => x.Classes)
+							.ThenInclude(x => x.Properties)
+							.ThenInclude(x => x.Attributes)
+							.First(x => x.Id == maidId);
+				if (File.Exists(Path.Combine(maid.Project.Path, ".git", "index.lock")) || File.Exists(Path.Combine(maid.Project.Path, ".git", "HEAD.lock")))
 				{
-					logger.LogInformation("指定分支为{breach},当前分支为{currentBreach},跳过本次执行", maid.Project.GitBranch, head);
+					logger.LogInformation("分支切换中,跳过本次执行");
 					return;
 				}
-			}
-			switch (maid.MaidWork)
-			{
-				case Models.CodeMaid.MaidWork.ConfigurationSync:
-					break;
-				case Models.CodeMaid.MaidWork.DtoSync:
-					break;
-				case Models.CodeMaid.MaidWork.HttpClientSync:
-					break;
-				case Models.CodeMaid.MaidWork.ControllerSync:
-					break;
-				case Models.CodeMaid.MaidWork.MasstransitConsumerSync:
-					break;
-				default:
-					break;
-			}
-			//检查更新
-			await MaidService.Update(maid, message.FilePath, message.IsDelete);
-			//如果有变化的话则发布变化事件
-			if (maidContext.ChangeTracker.Entries().Where(x => x.State != EntityState.Unchanged).Any())
-			{
-				await maidContext.SaveChangesAsync();
-				await context.Publish(new MaidChangeEvent() { MaidId = maid.Id });
+				if (!maid.Project.GitBranch.IsNullOrEmpty())
+				{
+					var head = File.ReadLines(Path.Combine(maid.Project.Path, ".git", "HEAD")).First();
+					if (!head.EndsWith(maid.Project.GitBranch))
+					{
+						logger.LogInformation("指定分支为{breach},当前分支为{currentBreach},跳过本次执行", maid.Project.GitBranch, head);
+						return;
+					}
+				}
+				switch (maid.MaidWork)
+				{
+					case Models.CodeMaid.MaidWork.ConfigurationSync:
+						break;
+					case Models.CodeMaid.MaidWork.DtoSync:
+						break;
+					case Models.CodeMaid.MaidWork.HttpClientSync:
+						break;
+					case Models.CodeMaid.MaidWork.ControllerSync:
+						break;
+					case Models.CodeMaid.MaidWork.MasstransitConsumerSync:
+						break;
+					default:
+						break;
+				}
+				//检查更新
+				await MaidService.Update(maid, message.FilePath, message.IsDelete);
+				//如果有变化的话则发布变化事件
+				if (maidContext.ChangeTracker.Entries().Where(x => x.State != EntityState.Unchanged).Any())
+				{
+					await maidContext.SaveChangesAsync();
+					await context.Publish(new MaidChangeEvent() { MaidId = maid.Id });
+				}
 			}
 			return;
 		}
